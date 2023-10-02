a59fb900cea4537412ecf56b8898768d407da3b6
Jannis Feldmann <jannis.feldmann@soxes.ch>
2023-10-02 09:05:17 +0200

    add pages

---
+++ app/additem/page.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="flex flex-col items-center gap-2">
+        <label htmlFor="item-name" className="text-lg font-semibold">
+          Enter Item Name:
+        </label>
+        <Input
+          id="item-name"
+          placeholder="Type item name here..."
+          width="300px"
+          size="large"
+        />
+        <div style={{ margin: "1rem 0" }}>
+          <Button auto color="primary">
+            Add Item
+          </Button>
+        </div>
+      </div>
+      <div className="text-center">
+        <h3>Recently Added Items</h3>
+        {/* Display recently added items here */}
+      </div>
+    </section>
+  );
+}
