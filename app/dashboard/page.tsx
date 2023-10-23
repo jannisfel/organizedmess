45665970da90d72cd9b91285c3883f20c11e5551
Silvio Brändle <silvio@silviobraendle.ch>
2023-10-23 10:30:49 +0200

    Fix Jannis mistake

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -60,32 +60,34 @@ export default function Home() {
             </p>
           </Link>
 
-        <Link
-          href="/additem"
-          className="block w-100 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-        >
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            List soon expiring items
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400 inline-flex items-center">
-            Click here to list soon expiring items
-            <svg
-              className="w-3.5 h-3.5 ml-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </p>
-        </Link>
-      </div>
-    </section>
+          <Link
+            href="/additem"
+            className="block w-100 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              List soon expiring items
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400 inline-flex items-center">
+              Click here to list soon expiring items
+              <svg
+                className="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </p>
+          </Link>
+        </div>
+      </section>
+    </ProtectRoute>
   );
+}
