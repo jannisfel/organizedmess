75a548c3d6f19e8154795b961256d38cee0229c2
Silvio Brändle <silvio@silviobraendle.ch>
2023-10-29 16:39:35 +0100

    Update site config links

--- config/site.ts
+++ config/site.ts
@@ -1,28 +1,35 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Organizedmess",
-	description: "Organizedmess",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
-		{
-			label: "Dashboard",
-			href: "/login",
-		},
-	],
-	navMenuItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
-		{
-			label: "Dashboard",
-			href: "/login",
-		},
-	],
-	links: {
-	},
+  name: "Organizedmess",
+  description: "Organizedmess",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Locations",
+      href: "/locations",
+    },
+    {
+      label: "Items",
+      href: "/items",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Locations",
+      href: "/locations",
+    },
+    {
+      label: "Items",
+      href: "/items",
+    },
+  ],
+  links: {},
 };
